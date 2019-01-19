843a4b2a8acad273449d7c0cbf59098bf578eeb6
steve <solise@mceesolutions.com>
2019-01-19 14:21:25 +0100

    first push/store request

---
+++ emeraldfb/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Emeraldfb",
+
+    'summary': """
+        Emeraldfb Modules""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "MCEE Solutions",
+    'website': "http://www.mceesolutions.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'emeraldfb',
+    'version': '0.1',
+
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','sale','website','hr','stock', ],
+
+    # always loaded
+    'data': [
+        'security/emeraldfb_security.xml',
+        'security/ir.model.access.csv',
+        'data/data.xml',
+        'views/views.xml',
+        'views/stock_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
---
+++ emeraldfb/models/stock.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+from datetime import date, timedelta
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, AccessError
+from odoo.tools import float_is_zero
+
+from functools import partial
+from odoo.tools.misc import formatLang
+
+from dateutil.relativedelta import relativedelta
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class Picking(models.Model):
+    _name = "stock.picking"
+    _inherit = 'stock.picking'
+    
+    location_id = fields.Many2one(
+        'stock.location', "Source Location",
+        default=lambda self: self.env['stock.picking.type'].browse(self._context.get('default_picking_type_id')).default_location_src_id,
+        readonly=False, required=True,
+        states={'draft': [('readonly', False)]})
+    location_dest_id = fields.Many2one(
+        'stock.location', "Destination Location",
+        default=lambda self: self.env['stock.picking.type'].browse(self._context.get('default_picking_type_id')).default_location_dest_id,
+        readonly=True, required=True,
+        states={'draft': [('readonly', False)]})
+    
+    @api.multi
+    def manager_confirm(self):
+        for order in self:
+            order.write({'man_confirm': True})
+        return True
+    
+    def _default_owner(self):
+        return self.env.context.get('default_employee_id') or self.env['res.users'].browse(self.env.uid).partner_id
+    
+    def _default_employee(self):
+        self.env['hr.employee'].search([('user_id','=',self.env.uid)])
+        return self.env['hr.employee'].search([('user_id','=',self.env.uid)])
+    
+    owner_id = fields.Many2one('res.partner', 'Owner',
+        states={'done': [('readonly', True)], 'cancel': [('readonly', True)]}, default=_default_owner,
+        help="Default Owner")
+    
+    employee_id = fields.Many2one('hr.employee', 'Employee',
+        states={'done': [('readonly', True)], 'cancel': [('readonly', True)]}, default=_default_employee,
+        help="Default Owner")
+    
+    man_confirm = fields.Boolean('Manager Confirmation', track_visibility='onchange')
+    net_lot_id = fields.Many2one(string="Serial Number", related="move_line_ids.lot_id", readonly=True)
+    internal_transfer = fields.Boolean('Internal Transfer?', track_visibility='onchange')
+    
+    @api.multi
+    def button_reset(self):
+        self.mapped('move_lines')._action_cancel()
+        self.write({'state': 'draft'})
+        return {}
+    
+    @api.model
+    def create(self, vals):
+        a = super(Picking, self).create(vals)
+        a.send_store_request_mail()
+        return a
+        return super(Picking, self).create(vals)
+    
+    @api.multi
+    def send_store_request_mail(self):
+        if self.state in ['draft','waiting','confirmed']:
+            group_id = self.env['ir.model.data'].xmlid_to_object('stock.group_stock_manager')
+            user_ids = []
+            partner_ids = []
+            for user in group_id.users:
+                user_ids.append(user.id)
+                partner_ids.append(user.partner_id.id)
+            self.message_subscribe_users(user_ids=user_ids)
+            subject = "A new store request {} has been made".format(self.name)
+            self.message_post(subject=subject,body=subject,partner_ids=partner_ids)
+            return False
+        return True
+    
+    @api.multi
+    def send_store_request_done_mail(self):
+        if self.state in ['done']:
+            subject = "Store request {} has been approved and validated".format(self.name)
+            partner_ids = []
+            for partner in self.sheet_id.message_partner_ids:
+                partner_ids.append(partner.id)
+            self.sheet_id.message_post(subject=subject,body=subject,partner_ids=partner_ids)
