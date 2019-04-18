585c32517b00cf703f9118c53f79bb005d8cf7d7
steve <solise@mceesolutions.com>
2019-04-18 07:39:26 +0100

    updates

--- emeraldfb/models/stock.py
+++ emeraldfb/models/stock.py
@@ -339,13 +339,13 @@ class PurchaseOrderLine(models.Model):
     _name = "purchase.order.line"
     _inherit = ['purchase.order.line']
     
-    discount = fields.Float(string='Discount')
+    discount = fields.Float(string='Discount', digits=dp.get_precision('Discount'), default=0.0)
     name = fields.Text(string='Description', required=True, store=True)
     
     @api.depends('product_qty', 'discount', 'price_unit', 'taxes_id')
     def _compute_amount(self):
         for line in self:
-            price = line.price_unit  - line.discount
+            price = line.price_unit  - (line.discount or 0.0)
             taxes = line.taxes_id.compute_all(price, line.order_id.currency_id, line.product_qty, product=line.product_id, partner=line.order_id.partner_id)
             line.update({
                 'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
