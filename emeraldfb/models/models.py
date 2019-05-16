d7ee03eefa9d5bae4c4c1425caa0b35a092c1077
steve <solise@mceesolutions.com>
2019-05-16 16:53:22 +0100

    recent updates

--- emeraldfb/models/models.py
+++ emeraldfb/models/models.py
@@ -378,6 +378,17 @@ class HrPayslipWorkedDays(models.Model):
     _inherit = 'hr.payslip.worked_days'
     
     number_of_days = fields.Float(string='Absent Days')
+ 
+   
+class PensionManager(models.Model):
+    _name = 'pen.type'
     
+    name = fields.Char(string='Name')
+    contact_person = fields.Char(string='Contact person')
+    phone = fields.Char(string='Phone Number')
+    contact_address = fields.Text(string='Contact Address')
+    pfa_id = fields.Char(string='PFA ID', required=True)
+    email = fields.Char(string='Email')
+    notes = fields.Text(string='Notes')
     
     
