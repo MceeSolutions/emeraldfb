8b3ba380d32862b8a260cf7f52bf62d46ab682e2
Olalekan Babawale <padinality@yahoo.com>
2020-12-02 09:57:53 +0000

    Add purchase request

---
+++ purchase_request/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Purchase Request",
+
+    'summary': """
+        Purchase request module""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "MCEE Business Solutions",
+    'website': "http://www.mceebusinesssolutions.com",
+
+    'category': 'Stock',
+    'version': '0.1',
+
+    'depends': ['base', 'stock', 'purchase', 'hr'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'data/sequence.xml',
+        'views/purchase_request_view.xml',
+        # 'views/templates.xml',
+    ],
+    
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
