824f7da46e41f06dcb7d12f152414d2f4043ad14
Olalekan Babawale <padinality@yahoo.com>
2021-01-12 07:00:47 +0000

    Add material and purchase request

--- material_requisition/models/__init__.py
+++ material_requisition/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import ir_request
-from . import ir_request_line
+# from . import ir_request_line
 from . import stock_picking
 from . import res_company
-from . import purchase_requisition
+# from . import purchase_requisition
