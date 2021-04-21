bb9dada6f1d32d62a58abda8a85ab4ef1c563645
Olalekan Babawale <padinality@yahoo.com>
2021-04-21 16:08:25 +0000

    Clean up references to payroll & leave

--- emeraldfb/__manifest__.py
+++ emeraldfb/__manifest__.py
@@ -12,14 +12,9 @@
     'author': "MCEE Solutions",
     'website': "http://www.mceesolutions.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'emeraldfb',
     'version': '0.47',
 
-
-    # any module necessary for this one to work correctly
     'depends': [
         'base',
         'sale',
@@ -28,12 +23,8 @@
         'stock',
         'purchase', 
         'hr_expense',
-        # 'hr_holidays',
-        # 'hr_payroll',
-        # 'netcom_hr_payroll',
     ],
-
-    # always loaded
+    
     'data': [
         'security/emeraldfb_security.xml',
         'security/ir.model.access.csv',
@@ -47,8 +38,5 @@
         'views/recruitment_view.xml',
         'views/customer_request_view.xml',
         'views/website_sale_templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
     ],
 }
--- sale_extension/models/sale.py
+++ sale_extension/models/sale.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    price_unit = fields.Float(readonly=True, string="XXPrice")    
+    price_unit = fields.Float(readonly=True)    
